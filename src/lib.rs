20612e60351b6ce0c47c6b5c77132aa7f871f4f1
takafumi-s <takafumi.s.dev@gmail.com>
2022-04-06 21:45:36 +0900

    test1-2

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
+    fn it_works_1() {
         let result = 2 + 2;
         assert_eq!(result, 4);
+    }
+    #[test]
+    fn it_works_2() {
+        let result = 1 + 1;
+        assert_eq!(result, 2);
     }
 }
